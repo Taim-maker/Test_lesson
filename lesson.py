3750408b20d0f673fd05e23134f9b1eac212404e
Taim-maker <btu24@icloud.com>
2021-09-03 20:58:33 +0300

    lesson2

---
+++ lesson.py
@@ -0,0 +1,20 @@
+# a = 15 #int
+# c = 3.7 #float
+# comp = complex(5,7)
+# print(type(comp))
+#
+# print(2 / 3)
+# print(int(2/2))
+# c = int(c)
+# print(c)
+# a = float(a)
+# print(a)
+
+
+# print(bin(a))
+# print(oct(a))
+# print(hex(a))
+# STRINGs
+a = 'Привет "Tom"'
+b = "Hello 'Tom'"
+print(a[-1])
